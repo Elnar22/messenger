518d7f8c7748a769605d57189311eda16b9efd78
Elnar22 <elnarkurbanov0@gmail.com>
2023-06-30 13:37:57 +0300

    connected redux thunk and local store through persist

--- src/screens/Chat/Chat.js
+++ src/screens/Chat/Chat.js
@@ -1,11 +1,11 @@
-import { useEffect, useMemo, useRef } from "react";
+import { useMemo } from "react";
 import { AUTHORS } from "../../utils/consts";
 import { MessageList } from "../../components/MessageList/messageList";
 import { Form } from "../../components/Form/Form";
 import { Navigate, useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { selectMessagesByChatId } from "../../store/messages/selectors";
-import { addMessage } from "../../store/messages/actions";
+import { addMessageWithReply } from "../../store/messages/actions";
 
 export function Chat() {
   const { id } = useParams();
@@ -14,11 +14,9 @@ export function Chat() {
   const messages = useSelector(getMessages);
   const dispatch = useDispatch();
 
-  const timeout = useRef();
-
   const sendMessage = (text) => {
     dispatch(
-      addMessage(
+      addMessageWithReply(
         {
           author: AUTHORS.human,
           text,
@@ -28,28 +26,6 @@ export function Chat() {
       )
     );
   };
-
-  useEffect(() => {
-    const lastMessage = messages?.[messages?.length - 1];
-    if (lastMessage?.author === AUTHORS.human) {
-      timeout.current = setTimeout(() => {
-        dispatch(
-          addMessage(
-            {
-              text: "Robot answer",
-              author: AUTHORS.robot,
-              id: `msg-${Date.now()}`,
-            },
-            id
-          )
-        );
-      }, 1000);
-    }
-
-    return () => {
-      clearTimeout(timeout.current);
-    };
-  }, [messages]);
 
   if (!messages) {
     return <Navigate to="/chat" replace />;
--- src/store/messages/actions.js
+++ src/store/messages/actions.js
@@ -1,3 +1,5 @@
+import { AUTHORS } from "../../utils/consts";
+
 export const ADD_MESSAGE = "MESSAGES::ADD_MESSAGE";
 export const INIT_MESSAGES_FOR_CHAT = "MESSAGES::INIT_MESSAGES_FOR_CHAT";
 export const CLEAR_MESSAGES_FOR_CHAT = "MESSAGES::CLEAR_MESSAGES_FOR_CHAT";
@@ -19,3 +21,25 @@ export const clearMessages = (chatId) => ({
   type: CLEAR_MESSAGES_FOR_CHAT,
   payload: chatId,
 });
+
+let timeout;
+export const addMessageWithReply = (newMsg, chatId) => (dispatch) => {
+  dispatch(addMessage(newMsg, chatId));
+
+  if (newMsg?.author === AUTHORS.human) {
+    clearTimeout(timeout);
+
+    timeout = setTimeout(() => {
+      dispatch(
+        addMessage(
+          {
+            text: "Robot answer",
+            author: AUTHORS.robot,
+            id: `msg-${Date.now()}`,
+          },
+          chatId
+        )
+      );
+    }, 1000);
+  }
+};
